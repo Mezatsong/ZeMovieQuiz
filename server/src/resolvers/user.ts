4edfe3b5ffbc1ff14828f6fbc02bb19f642d3463
Ben MEZATSONG <meztsacar@gmail.com>
2022-07-25 23:59:46 +0100

    feat(api); hash password before saving in register

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,12 +1,15 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../entities/User";
 import { UserInput } from "../types";
+import crypto from 'crypto';
 
 @Resolver()
 export class userResolver {
   @Mutation(() => User)
   async register(@Arg("input") input: UserInput): Promise<User | undefined> {
-    return User.create(input).save();
+    const { password } = input;
+    const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+    return User.create({...input, password: hashedPassword}).save();
   }
 
   @Query(() => User, { nullable: true })
