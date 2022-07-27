baa3669e3907b604b94a42b818fb3ce5741f4219
Ben MEZATSONG <meztsacar@gmail.com>
2022-07-27 06:43:53 +0100

    fix(web): timer in TimerLabel now work fine

--- web/src/components/play/TimerLabel.tsx
+++ web/src/components/play/TimerLabel.tsx
@@ -11,25 +11,29 @@ export const TimerLabel: FC<ITimeLabelProps> = (props) => {
   const [remainingSeconds, setRemainingSeconds] = useState(props.timeInSeconds);
 
   useEffect(() => {
-    passTime();
+    passTime(remainingSeconds+1);
   }, []);
 
-  const passTime = () => {
-    if (remainingSeconds > 0) {
-      setRemainingSeconds(remainingSeconds - 1);
-      setTimeout(passTime, 1000);
+  const passTime = (sec: number) => {
+    if (sec > 0) {
+      setRemainingSeconds(sec - 1);
+      setTimeout(() => passTime(sec-1), 1000);
     } else {
       props.onTimeOver();
     }
   }
 
-  const lastSeconds = props.timeInSeconds / 100;
+  const lastSeconds = props.timeInSeconds / 10;
   const color = remainingSeconds > lastSeconds ? "black" : "red";
 
   return (
     <Box m={4} display="flex" alignItems="center" justifyContent="center">
-      <Text fontSize="lg" fontWeight="bold" color={color}>{remainingSeconds}</Text>
-      <Text fontSize="sm" fontWeight="bold" color={color} ml="3">seconds left</Text>
+      {remainingSeconds ? (
+        <>
+          <Text fontSize="lg" fontWeight="bold" color={color}>{remainingSeconds}</Text>
+          <Text fontSize="sm" fontWeight="bold" color={color} ml="3">seconds left</Text>
+        </>
+      ) : <Text fontSize="sm" fontWeight="bold" ml="3">Time over</Text>}
     </Box>
   );
 }
